38a9a62da4c1a6a348d895319a8ae5b566cb1602
DarryDee <darryd04@gmail.com>
2024-01-04 20:03:32 +0100

    Coded 4 main methods which should run in the browser compiler because of prompt and alert

---
+++ index.js
@@ -0,0 +1,46 @@
+function getComputerChoice(){
+
+    let number = Math.floor(Math.random() * 3);
+
+    return (number === 0) ? "rock" : (number === 1) ? "paper":  "scissors";
+}
+
+function getPlayerChoice(){
+
+    let choice;
+    for(let i = 0; i < 5;i++){
+        
+     choice = prompt("Choose rock, paper or scissors","default");
+    
+     if(choice.toLowerCase() !== "paper" && choice.toLowerCase() !== "rock" && choice.toLowerCase() !== "scissors")
+     {
+        alert("Give in rock, paper or scissors");
+        if(i === 5) alert("bro.....");
+     }
+
+     else{
+        break;
+     }
+    }
+    return choice;
+}
+
+playRound(getPlayerChoice(), getComputerChoice());
+
+function playRound(playerSelection, computerSelection) {
+    let playerchoice = playerSelection.toLowerCase();
+
+    (playerchoice === computerSelection) ? console.log("It`s a tie"):
+    ((playerchoice === "paper")&&(computerSelection === "rock")) ? console.log("PLAYER WON!") :
+    ((playerchoice === "scissors")&&(computerSelection === "paper")) ? console.log("PLAYER WON!"):
+    console.log("COMPUTER WON!");
+    
+} 
+
+game();
+function game(){
+
+    for(let i = 0; i <= 4; i++){
+        playRound(getPlayerChoice(), getComputerChoice());
+    }
+}
