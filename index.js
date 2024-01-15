76c82efe023142aa27208d21cb7588a1cf08ec9b
DarryDee <darryd04@gmail.com>
2024-01-15 22:22:06 +0100

    Added background color to the images in order to show the winner.
    The PlayRoundMethod returns the winnner to the eventhandler to change the element attached to it.

--- index.js
+++ index.js
@@ -1,20 +1,97 @@
-const playerRock = document.querySelector("#Player-Rock"); 
-const playerPaper = document.querySelector("#Player-Paper");
-const playerScissors = document.querySelector("#Player-Scissors");
+const playerRock = document.querySelector(".Player-Rock"); 
+const playerPaper = document.querySelector(".Player-Paper");
+const playerScissors = document.querySelector(".Player-Scissors");
 
-const choice = document.querySelector("#choice");
+const BotRock = document.querySelector(".Computer-Rock"); 
+const BotPaper = document.querySelector(".Computer-Paper");
+const BotScissors = document.querySelector(".Computer-Scissors");
 
-playerRock.addEventListener("click", ()=>{
+const webpage = document.getElementById("webpage");
+const buttons = document.querySelectorAll("#button");
 
-    playRound("Rock",getComputerChoice())
+let buttonColored = false;
+playerRock.addEventListener("click", (e)=>{
+
+    /*
+    if(buttonColored) e.preventDefault();
+    */
+    const decision = playRound("Rock",getComputerChoice())
+    console.log(decision);
+    if(decision){   
+        playerRock.style.background = "green";
+        BotScissors.style.background = "red";
+        buttonColored = true;
+    }
+    else if(decision === 0){
+ 
+        playerRock.style.background = "Aqua";
+        BotRock.style.background = "Aqua";
+
+    }
+    else{
+
+        playerRock.style.background = "red";
+        BotPaper.style.background = "green"; 
+    }
+
+    setTimeout(() => {
+        buttons.forEach(buttons => {
+            buttons.style.background = "transparent";
+            /*
+            buttonColored = false;
+            preventDefaultEnabled = false;
+            */
+        })
+    },1000)
+
 })
 playerPaper.addEventListener("click", ()=>{
 
-    playRound("Paper",getComputerChoice())
-})
-playerScissors.addEventListener("click", ()=>{
+    const decision = playRound("Paper",getComputerChoice())
 
-    playRound("Scissors",getComputerChoice())
+    if(decision){
+        
+        playerPaper.style.background = "green";
+        BotRock.style.background = "red";
+    }
+
+    else if(decision === 0){
+        playerPaper.style.background = "Aqua";
+        BotPaper.style.background = "Aqua";
+    }
+    else{
+        playerPaper.style.background = "red";
+        BotScissors.style.background = "green";
+    }
+    setTimeout(() => {
+        buttons.forEach(buttons => {
+            buttons.style.background = "transparent";
+        })
+    },1000)
+})
+
+playerScissors.addEventListener("click", (e) => {
+
+    const decision =  playRound("Scissors",getComputerChoice())
+
+    if(decision){
+        playerScissors.style.background = "green";
+        BotPaper.style.background = "red";
+    }
+
+    else if(decision === 0){
+        playerScissors.style.background = "Aqua";
+        BotScissors.style.background = "Aqua";
+    }
+    else{
+        playerScissors.style.background = "red";
+        BotRock.style.background = "green";
+    }
+    setTimeout(() => {
+        buttons.forEach(buttons => {
+            buttons.style.background = "transparent";
+        })
+    },1000)
 })
 function getComputerChoice(){
 
@@ -23,23 +100,17 @@ function getComputerChoice(){
     return (number === 0) ? "rock" : (number === 1) ? "paper":  "scissors";
 }
 
-playRound(getPlayerChoice(), getComputerChoice());
-
 function playRound(playerSelection, computerSelection) {
-    let playerchoice = playerSelection.toLowerCase();
+
+    let isPlayerWinning = true;
 
     console.log(computerSelection);
-
-    (playerchoice === computerSelection) ? console.log("It`s a tie"):
-    ((playerchoice === "paper")&&(computerSelection === "rock")) ? console.log("PLAYER WON!") :
-    ((playerchoice === "scissors")&&(computerSelection === "paper")) ? console.log("PLAYER WON!"):
-    console.log("COMPUTER WON!");
+    
+    let playerchoice = playerSelection.toLowerCase();
+    if (playerchoice === computerSelection) return 0; 
+    else if ((playerchoice === "rock")&&(computerSelection === "scissors"))   return isPlayerWinning; 
+    else if ((playerchoice === "scissors")&&(computerSelection === "paper"))  return isPlayerWinning;
+    else if ((playerchoice === "paper")&&(computerSelection === "rock"))  return isPlayerWinning;
+    else return false;
     
 } 
-
-game();
-function game(){
-
-    playRound(getPlayerChoice(), getComputerChoice())
-    
-}
